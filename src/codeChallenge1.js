1f68038d9052104f8a8fb3063f1ddc4b74fd5526
Cristian Montes <cmontesmujica@gmail.com>
2021-08-10 11:01:47 -0700

    double,stringitup, capitalize names and names only tested and passed

--- src/codeChallenge1.js
+++ src/codeChallenge1.js
@@ -1,3 +1,30 @@
-export function doubleNumbers(arr){
+//DOUBLE NUMBERS
+export   function doubleNumbers(arr){
     return arr.map((num) => num *2 );
 }
+
+//STRING IT UP
+export function stringItUp(arr){
+    return arr.map((num) => num.toString());
+  
+}
+
+// CAPITALIZE NAMES
+export function capitalizeNames(arr){
+ const loArr = arr.map((string) => {
+      return string.toLowerCase();
+  })
+  return loArr.map((firtCap) => {
+      return firtCap[0].toUpperCase()+ firtCap.slice(1);
+  })
+}
+
+//NAMES ONLY
+export function namesOnly(arr){
+    return arr.map((famousNames) => {
+        return famousNames.name;
+    })
+  
+}
+
+// MAKES STRINGSq
--- src/codeChallenge1.test.js
+++ src/codeChallenge1.test.js
@@ -1,7 +1,51 @@
-import doubleNumbers from './codeChallenge1';
+import {doubleNumbers, stringItUp, capitalizeNames, namesOnly} from './codeChallenge1';
 
+//DOUBLE NUMBERS
 test('doubles numbers', () => {
   const input = [2, 5, 100]; // arrange
   const output = doubleNumbers(input); // act
   expect(output).toEqual([4, 10, 200]); // assert
 });
+
+//STRING IT UP
+test('string it up', () => {
+    const input = [2, 5, 100]; // arrange
+    const output = stringItUp(input); // act
+    expect(output).toEqual(['2', '5', '100']); // assert
+  });
+
+// CAPITALIZE NAMES
+  test('capitalize names', () => {
+    const input = ["john", "JACOB", "jinGleHeimer", "schmidt"]; // arrange
+    const output = capitalizeNames(input); // act
+    expect(output).toEqual(["John", "Jacob", "Jingleheimer", "Schmidt"]); // assert
+  });
+
+  //NAMES ONLY
+  test('names only', () => {
+    const input = [
+        {
+            name: "Angelina Jolie",
+            age: 80
+        },
+        {
+            name: "Eric Jones",
+            age: 2
+        },
+        {
+            name: "Paris Hilton",
+            age: 5
+        },
+        {
+            name: "Kayne West",
+            age: 16
+        },
+        {
+            name: "Bob Ziroll",
+            age: 100
+        }
+    ]
+    ; // arrange
+    const output = namesOnly(input); // act
+    expect(output).toEqual([ "Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]); // assert
+  });
