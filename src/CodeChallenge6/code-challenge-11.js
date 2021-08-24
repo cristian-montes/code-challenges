71794176ca30242ecc531943b4748c273c3f500f
Cristian Montes <cmontesmujica@gmail.com>
2021-08-24 09:02:46 -0700

    working onstore cc

--- src/CodeChallenge6/code-challenge-11.js
+++ src/CodeChallenge6/code-challenge-11.js
@@ -8,3 +8,20 @@ export function findMax(matrix) {
     })
     return Math.max(...arrOfLarge);
 }
+
+export function totalSum(matrix) {
+ const flatttie = matrix.flatMap(subArr => subArr);
+ return flatttie.reduce((a ,b) => a+b)
+}
+
+
+
+export function grandTotal(stores){
+    const hoursOpen = ['9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.'];
+
+    return stores.map((subArr) => {
+        for(let item of subArr){
+            return item;
+        }
+    })
+}
